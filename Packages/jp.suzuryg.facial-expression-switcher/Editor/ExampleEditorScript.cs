728e491b36c37d4991790446e1cb3135808a6633
suzuryg <suzuryg@outlook.com>
2022-10-16 02:11:55 +0900

    chore: Add UniRx as submodule

--- Packages/jp.suzuryg.facial-expression-switcher/Editor/ExampleEditorScript.cs
+++ Packages/jp.suzuryg.facial-expression-switcher/Editor/ExampleEditorScript.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEditor;
 using Newtonsoft.Json;
+using UniRx;
 
 public class ExampleEditorScript
 {
+    public ReactiveProperty<string> ExampleProperty { get; }
+
     [MenuItem("Example Editor Script/Test")]
     static void Test()
     {
