85062f728ad5d158bd36b7efa967c829e196edc0
suzuryg <suzuryg@outlook.com>
2023-07-16 02:27:09 +0900

    chore: Change hierarchy to shorten path letters (UniRx)

--- External/package-bundler/setting_unirx.py
+++ External/package-bundler/setting_unirx.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 src_dir = Path('../UniRx/Assets/Plugins/UniRx/Scripts')
-dst_dir = Path('../../Packages/jp.suzuryg.face-emo/External/neuecc/UniRx/Runtime')
+dst_dir = Path('../../Packages/jp.suzuryg.face-emo/Ext/UniRx/Runtime')
 ignore_dirs = []
 target_extensions = ['.cs',]
 gitignore_content = '''*.cs
