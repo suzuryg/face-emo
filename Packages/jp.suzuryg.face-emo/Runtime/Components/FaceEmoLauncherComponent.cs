23e645c99fde76a31aa979c222b6f5a28a03c77a
suzuryg <suzuryg@outlook.com>
2023-09-26 21:46:17 +0900

    chore: Display fields of FaceEmoLauncherComponent in dev environment

--- Packages/jp.suzuryg.face-emo/Runtime/Components/FaceEmoLauncherComponent.cs
+++ Packages/jp.suzuryg.face-emo/Runtime/Components/FaceEmoLauncherComponent.cs
@@ -6,13 +6,44 @@ namespace Suzuryg.FaceEmo.Components
 {
     public class FaceEmoLauncherComponent : MonoBehaviour
     {
-        [HideInInspector] public int InstanceId;
-        [HideInInspector] public AV3Setting AV3Setting;
-        [HideInInspector] public ThumbnailSetting ThumbnailSetting;
-        [HideInInspector] public ExpressionEditorSetting ExpressionEditorSetting;
-        [HideInInspector] public HierarchyViewState HierarchyViewState;
-        [HideInInspector] public MenuItemListViewState MenuItemListViewState;
-        [HideInInspector] public ViewSelection ViewSelection;
-        [HideInInspector] public InspectorViewState InspectorViewState;
+#if !SHOW_FACE_EMO_FIELDS
+        [HideInInspector]
+#endif
+        public int InstanceId;
+
+#if !SHOW_FACE_EMO_FIELDS
+        [HideInInspector]
+#endif
+        public AV3Setting AV3Setting;
+
+#if !SHOW_FACE_EMO_FIELDS
+        [HideInInspector]
+#endif
+        public ThumbnailSetting ThumbnailSetting;
+
+#if !SHOW_FACE_EMO_FIELDS
+        [HideInInspector]
+#endif
+        public ExpressionEditorSetting ExpressionEditorSetting;
+
+#if !SHOW_FACE_EMO_FIELDS
+        [HideInInspector]
+#endif
+        public HierarchyViewState HierarchyViewState;
+
+#if !SHOW_FACE_EMO_FIELDS
+        [HideInInspector]
+#endif
+        public MenuItemListViewState MenuItemListViewState;
+
+#if !SHOW_FACE_EMO_FIELDS
+        [HideInInspector]
+#endif
+        public ViewSelection ViewSelection;
+
+#if !SHOW_FACE_EMO_FIELDS
+        [HideInInspector]
+#endif
+        public InspectorViewState InspectorViewState;
     }
 }
