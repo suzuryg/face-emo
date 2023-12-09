2907ce9aceb3f28fdc0dc470183c8738a22d4478
suzuryg <suzuryg@outlook.com>
2023-12-09 16:34:56 +0900

    fix: Replace Zenject-usage.dll with source code
    Failed to load Zenject-usage.dll in Zenject 9.2.0 when using VRCSDK 3.5.0.
    To avoid compatibility issues, replace it with source code.

---
+++ External/package-bundler/setting_extenject_usage.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+src_dir = Path('../Extenject/AssemblyBuild/Zenject-usage')
+dst_dir = Path('../../Packages/jp.suzuryg.face-emo/Ext/Zenject/a/u')
+ignore_dirs = []
+target_extensions = ['.cs',]
+gitignore_content = '''*.cs
+*.meta
+!*.asmdef.meta
+'''
