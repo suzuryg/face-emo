44ca80cd5fb55e5481a45268eec1ded75a886c82
suzuryg <suzuryg@outlook.com>
2023-12-09 19:04:42 +0900

    chore: Exclude JetbrainsAnnotations in Extenject to suppress warnings

--- External/package-bundler/setting_extenject_usage.py
+++ External/package-bundler/setting_extenject_usage.py
@@ -3,6 +3,7 @@ from pathlib import Path
 src_dir = Path('../Extenject/AssemblyBuild/Zenject-usage')
 dst_dir = Path('../../Packages/jp.suzuryg.face-emo/Ext/Zenject/a/u')
 ignore_dirs = []
+ignore_files = ['JetbrainsAnnotations.cs']
 target_extensions = ['.cs',]
 gitignore_content = '''*.cs
 *.meta
