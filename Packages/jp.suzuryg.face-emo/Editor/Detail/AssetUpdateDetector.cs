c98633aed17f2c83186b1acb7ba9000ab3cbf1b2
suzuryg <suzuryg@outlook.com>
2024-02-17 21:16:12 +0900

    perf: Improve performance of AssetUpdateDetector

--- Packages/jp.suzuryg.face-emo/Editor/Detail/AssetUpdateDetector.cs
+++ Packages/jp.suzuryg.face-emo/Editor/Detail/AssetUpdateDetector.cs
@@ -1,30 +1,30 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using UniRx;
 using System.Linq;
+using UniRx;
+using UnityEditor;
 
 public class AssetUpdateDetector : AssetPostprocessor
 {
-    public static IObservable<List<string>> OnAnimationClipUpdated => _onAnimationClipUpdated.AsObservable();
+    public static IObservable<IEnumerable<string>> OnAnimationClipUpdated => _onAnimationClipUpdated.AsObservable();
 
-    public static Subject<List<string>> _onAnimationClipUpdated = new Subject<List<string>>();
+    public static Subject<IEnumerable<string>> _onAnimationClipUpdated = new Subject<IEnumerable<string>>();
 
     private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
     {
-        foreach (string assetPath in importedAssets)
+        if (importedAssets is null)
         {
-            var list = new List<string>();
-            if (AssetDatabase.LoadAssetAtPath<AnimationClip>(assetPath) != null)
-            {
-                list.Add(AssetDatabase.AssetPathToGUID(assetPath));
-            }
+            return;
+        }
 
-            if (list.Any())
-            {
-                _onAnimationClipUpdated.OnNext(list);
-            }
+        var clips = importedAssets
+            .Where(x => x?.ToLower()?.EndsWith(".anim") == true)
+            .Select(x => AssetDatabase.AssetPathToGUID(x))
+            .Where(x => !string.IsNullOrEmpty(x));
+
+        if (clips.Any())
+        {
+            _onAnimationClipUpdated.OnNext(clips);
         }
     }
 }
