1a5530e60f166cdcc5216518db6d95a5ae2435ef
suzuryg <suzuryg@outlook.com>
2025-10-12 10:31:45 +0900

    ui: Update message to refer to tree view instead of left panel

--- Packages/jp.suzuryg.face-emo/Editor/Detail/Localization/LocalizationTable.cs
+++ Packages/jp.suzuryg.face-emo/Editor/Detail/Localization/LocalizationTable.cs
@@ -95,7 +95,7 @@ namespace Suzuryg.FaceEmo.Detail.Localization
         public string MenuItemListView_Blink = "Enable Blink";
         public string MenuItemListView_MouthMorphCanceler = "Mouth Morph Canceler";
         public string MenuItemListView_Empty = "You can add an expression pattern by pressing the button\nat the top of the view.";
-        public string MenuItemListView_SelectFolder = "Please select a folder on the left panel.";
+        public string MenuItemListView_SelectFolder = "Please select a folder from the tree view.";
 
         public string BranchListView_Title = "Expressions";
         public string BranchListView_Simplify = "Simplify";
