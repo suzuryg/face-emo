78f0aaeaa0092c9c749f288580a2675a343e1111
suzuryg <77635213+suzuryg@users.noreply.github.com>
2026-03-01 00:16:22 +0900

    Add detection logic for FX disable switch

---
+++ Packages/jp.suzuryg.face-emo/Editor/Detail/AV3/Importers/FxDisableSwitchDetector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using VRC.SDK3.Avatars.Components;
+using VRC.SDKBase;
+using Debug = UnityEngine.Debug;
+
+namespace Suzuryg.FaceEmo.Detail.AV3.Importers
+{
+    internal static class FxDisableSwitchDetector
+    {
+        public static bool Detect(VRCAvatarDescriptor avatarDescriptor)
+        {
+            try
+            {
+                var fx = ImportUtility.GetFxLayer(avatarDescriptor);
+                if (fx == null) return false;
+
+                foreach (var layer in fx.layers)
+                {
+                    foreach (var state in layer.stateMachine.states)
+                    {
+                        foreach (var behaviour in state.state.behaviours)
+                        {
+                            if (behaviour is VRCPlayableLayerControl control &&
+                                control.layer == VRC_PlayableLayerControl.BlendableLayer.FX &&
+                                control.goalWeight == 0)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("[FaceEmo] Failed to detect FxDisableSwitch. " + ex);
+                return false;
+            }
+        }
+    }
+}
